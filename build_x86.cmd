16a62ce8c72ef4d79a7686ac428d9baf45a60a60
Gsemia <gsemiaspam@gmail.com>
2012-04-08 14:14:40 +0200

    replaced python 32 bit with environment variable

--- build_x86.cmd
+++ build_x86.cmd
@@ -1,8 +1,9 @@
 del dist\console.exe 
 del dist-x86\ns2update.exe
 rmdir /s /q build
+call env.cmd
 
-f:\tools\python27_x86\python pyinstaller-1.5.1-x86/Build.py console.spec
+%python27_32% pyinstaller-1.5.1-x86/Build.py console.spec
 
 move dist\console.exe dist-x86\ns2update.exe
 
